e569412eeeabfc82e9f84e092ff92466b94a86df
martik32 <kieran.martin@roche.com>
2017-08-31 13:35:15 +0200

    add mode and factor comparison checks

--- tests/testthat/test-core.R
+++ tests/testthat/test-core.R
@@ -26,16 +26,35 @@ set.seed(101010223)
 TDAT2 <- TDAT %>%
     mutate( INTEGER = ifelse(runif(nrow(.)) >0.8 , INTEGER + 1 , INTEGER))
 
+TDAT_MODECHANGE <- TDAT %>%
+  mutate( INTEGER = as.character(INTEGER))
+
+
+TDAT_FACTCHANGE <- TDAT %>%
+  mutate( CATEGORICAL = factor(CATEGORICAL, levels = c(levels(CATEGORICAL), 'New')))
+
+#switch integer to double, this shouldn't affect the comparison
+TDAT_MODEDBL <- TDAT %>%
+  mutate( INTEGER = as.double(INTEGER))
 
 test_that( "Check comparision of equal objects",{
     expect_false( rcompare(iris, iris)$Issues )
     expect_false( rcompare(TDAT, TDAT)$Issues )
     expect_false( rcompare(TDAT, TDAT, "ID")$Issues )
     expect_false( rcompare(TDAT, TDAT, c("GROUP1" , "GROUP2"))$Issues )
+    expect_false( rcompare(TDAT, TDAT_MODEDBL)$Issues )
 })
 
 test_that( "Unequal objects error" , {
     expect_warning( rcompare(TDAT , TDAT2), 'Not all values compared equal' )
+})
+
+test_that( "Differing modes error" , {
+  expect_warning( rcompare(TDAT , TDAT_MODECHANGE ), 'There are Columns in BASE and COMPARE with different modes' )
+})
+
+test_that( "Differing factor levels error" , {
+  expect_warning( rcompare(TDAT , TDAT_FACTCHANGE ), 'There are Factor Columns in BASE and COMPARE with different levels' )
 })
 
 
