492554d9fcf7fa1377ed746e04f18377f965786a
martik32 <kieran.martin@roche.com>
2017-10-26 09:22:24 +0200

    assign classes to each rcompare object

--- R/misc_functions.R
+++ R/misc_functions.R
@@ -12,4 +12,21 @@ fix_factor_vars <- function( dsin , vars){
 }
 
 
+class_adder <- function(objectin, new_class, message, checkfun, order){
+  class(objectin) <- append(new_class, class(objectin))
+  attr(objectin, 'message') <- message
+  attr(objectin, 'checkfun') <- checkfun
+  attr(objectin, 'order') <- order
+  objectin
+}
 
+nonempty_list <- function(in_list){
+  noenmpty_list <- in_list[map(in_list, nrow)>0]
+  noenmpty_list
+}
+
+checklength <- function(in_list){
+  length(nonempty_list(in_list))
+}
+
+get_attrib <- function()
