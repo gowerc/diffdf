d043f9037d11ed9e49845dca394bc58d95e44b80
martik32 <kieran.martin@roche.com>
2017-11-17 12:47:07 +0100

    add tolerances to address issue #31

--- tests/testthat/test-is_different.R
+++ tests/testthat/test-is_different.R
@@ -1,7 +1,7 @@
 
 context("Testing is_different")
 
-test_that( "is_different correclty doesn't flag identical objects", {
+test_that( "is_different correctly doesn't flag identical objects", {
     
     expect_self <- function(x , name)  {
         expect_true( !is_different( x , x) %>%  all, label = name ) 
@@ -20,6 +20,15 @@ test_that( "is_different correctly flags differences", {
     expect_equal( is_different( VALS$chr, VALS$chr_na) , c(F,F,F,T,T))
     expect_equal( is_different( VALS$fct, VALS$fct_na) , c(F,F,T,F,F))
     expect_equal( is_different( VALS$lgl, VALS$lgl_na) , c(F,F,T,F,F))
+})
+
+test_that("is_different correctly uses tolerances/scale arguments",{
+    expect_equal( is_different( VALS$flt, VALS$flt2) , c(F,F,F,F,F))
+    expect_equal( is_different( VALS$flt, VALS$flt2, tolerance = 0.000000000001 ) , c(T,T,T,T,T)) 
+    expect_equal( is_different( VALS$flt, VALS$flt2, tolerance = 1,
+                                scale = 1e-13) , c(T,T,T,T,T)) 
+    expect_equal( is_different( VALS$flt, VALS$flt_calc) , c(F,F,F,F,F)) 
+    expect_equal( is_different( VALS$flt, VALS$flt_calc, tolerance = 1e-15) , VALS$flt!=VALS$flt_calc) 
 })
     
 
