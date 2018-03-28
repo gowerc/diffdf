2f6f961fa133990bdf774d2a3e67a8f7c503463e
martik32 <kieran.martin@roche.com>
2018-03-28 16:14:43 +0200

    add basic documentation

--- R/issues.R
+++ R/issues.R
@@ -1,41 +1,67 @@
-
+#' construct_s3
+#' 
+#' Make an s3 object with added s3 class, preserving other arguments
+#' @param Class new class to provide to bject
+#' @param .... Other arguments
 construct_s3 <- function(Class , ...){
     x <- list(...)
     class(x) <- Class
     return(x)
 }
 
-
+#' get_text
+#' 
+#' Get the required text depending on type of issue
+#' @param object inputted object of class issue
+#' @param ... other arguments
 get_text <- function (object, ...) {
     UseMethod("get_text", object)
 }
 
+#' get_issue_message
+#' 
+#' Simple function to grab the issue message
+#' @param object inputted object of class issue
+#' @param ... other arguments
 get_issue_message <- function (object, ...) {
     UseMethod("get_issue_message", object)
 }
 
+
+#' get_issue_message
+#' 
+#' Simple function to grab the print message
+#' @param object inputted object of class issue
+#' @param ... other arguments
 get_print_message <- function (object, ...) {
     UseMethod("get_print_message", object)
 }
 
+
+#' has_issue_message
+#' 
+#' Determine if a function has an issue
+#' @param object inputted object of class issue
+#' @param ... other arguments
 has_issue <- function (object, ...) {
     UseMethod("has_issue", object)
 }
 
 
-#' Title
+#' has_issue.default
 #' 
-#' Description
-#' @param object TODO
+#' Default method for default objects, just returns true
+#' @param object Inputted object
 has_issue.default <- function(object) TRUE
 
 
 
 
-#' Title
+#' get_print_message.default
 #' 
-#' Description
-#' @param object TODO
+#' Default method for geting print method. Checks if the object has an issue
+#' And returns the text. Otherwise returns NULL
+#' @param object inputted object
 get_print_message.default <- function(object){
     if( has_issue(object) ){  
         get_text(object)
@@ -45,10 +71,11 @@ get_print_message.default <- function(object){
 }
 
 
-#' Title
+#' get_issue_message.default
 #' 
-#' Description
-#' @param object TODO
+#' Default method for geting issue message. Checks if the object has an issue
+#' And returns the message. Returns a blank otherwise
+#' @param object inputted object
 get_issue_message.default <- function(object){
     if ( has_issue(object) ) {
         return( object$message)
@@ -57,12 +84,12 @@ get_issue_message.default <- function(object){
     }
 }
 
-#' Title
+#' get_text.default
 #' 
-#' Description
-#' @param object TODO
-#' @param dsin TODO
-#' @param row_limit TODO
+#' Default method for getting text, generates ascii output for an issue
+#' @param object issue
+#' @param dsin dataset provided by issue
+#' @param row_limit Maximum number of rows displayed in dataset
 get_text.default <- function(object, dsin , row_limit = 10){
         
     if( nrow(dsin) == 0 ) {
@@ -106,12 +133,19 @@ get_text.default <- function(object, dsin , row_limit = 10){
 
 
 ######  Basic issues 
-
+#' has_issue.issue_basic
+#'
+#'Check whether a basic issue has some problems by checking nrows in the object
+#' @param object an object of class issue_basic
 has_issue.issue_basic <-  function(object){
     nrow(object$value) > 0
 }
 
-
+#' get_text.issue_basic
+#'
+#' Get text from a basic issue, based on the class of the value of the issue
+#'
+#' @param object an object of class issue_basic
 get_text.issue_basic <- function(object){
     NextMethod(dsin = object$value)
 }
@@ -119,20 +153,21 @@ get_text.issue_basic <- function(object){
 
 ######  List of Issues
 
-#' Title
+#' has_issue.issue_list
 #' 
-#' Description
-#' @param object TODO
+#' Determine if a list issue has any issues, looks through each object in turn and apply
+#' has issue
+#' @param object an issue_list object
 #' @importFrom purrr map_lgl
 has_issue.issue_list <- function(object){
     num_issues =  map_lgl( object$value , function(x) has_issue(x) ) %>% any
     return( num_issues )
 }
 
-#' Title
+#' get_text.issue_list
 #' 
-#' Description
-#' @param object TODO
+#' Get the text for a list object, by applying get_text over the list
+#' @param object an object of class issue_list
 #' @importFrom purrr map
 get_text.issue_list <- function(object){
     map( object$value , function(x) get_text(x))
@@ -143,16 +178,19 @@ get_text.issue_list <- function(object){
 
 ###### Vector as an Issue
 
-
+#' has_issue.issue_vector
+#' 
+#' Determine if a vector issue has any issues, by summing over value
+#' @param object an issue_vector object
 has_issue.issue_vector <-  function(object){
     sum(object$value) > 0
 }
 
 
-#' Title
+#' get_text.issue_vector
 #' 
-#' Description
-#' @param object TODO
+#' Generate text for an issue_vector object, adds text at top then applies default method
+#' @param object an issue_vector object
 #' @importFrom tibble rownames_to_column
 get_text.issue_vector <- function(object){
     datin_tibble <- object$value %>% 
