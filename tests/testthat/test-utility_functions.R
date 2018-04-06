091ae1f5bfba8d54bfda7cfd9862410dc9eca165
gowerc <craig.gower@roche.com>
2018-04-06 10:53:47 +0200

    added tests for has_issue

---
+++ tests/testthat/test-utility_functions.R
@@ -0,0 +1,79 @@
+
+
+
+
+
+
+context("Utility Functions")
+
+
+test_that( "rcompare_has_issue works as expected"){
+    
+    ### Note that the first 2 comparisons in list_of_datasets are identical so we expect true
+    ### all others are different so we expect false
+    
+    for ( i in length(RES)){
+        x <- rcompare(
+            list_of_comparisons[[i]][[1]] , 
+            list_of_comparisons[[i]][[2]] , 
+            suppress_warnings = T
+        ) 
+        if ( i %in% c(1,2)) {
+            expect_false(rcompare_has_issue(x))
+        } else {
+            expect_true( rcompare_has_issue(x))
+        }
+        
+    }
+    
+    ## Testing with keys
+    comp <- list_of_comparisons[["everything"]]
+    expect_true(
+        rcompare_has_issue(
+            rcompare( comp[[1]] ,  comp[[2]] , keys = c("ID" , "GROUP1"), suppress_warnings = T)  
+        )
+    )
+    
+    expect_false(
+        rcompare_has_issue(
+            rcompare( TDAT ,  TDAT , keys = c("ID" , "GROUP1"), suppress_warnings = T)  
+        )
+    )
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
