b63e207d864a882f59241ec288a287eca65d39d0
martik32 <kieran.martin@roche.com>
2021-04-09 13:16:56 +0000

    suggested modification

--- R/render.R
+++ R/render.R
@@ -5,13 +5,6 @@ render <- R6::R6Class(
         strings = NULL,
         
         valid_content_types = c(
-            "h1",
-            "h2",
-            "h3", 
-            "h4",
-            "p",
-            "br",
-            "table"
         ),
         
         initialize = function(display, rowlimit = 10){
@@ -21,19 +14,9 @@ render <- R6::R6Class(
             
             for( i in seq_along(display_flat)){
                 item_content <- display_flat[[i]]$content
-                item_type <- display_flat[[i]]$type
-                stopifnot(item_type %in% self$valid_content_types)
+                item_type <- self$class_handle(display_flat[[i]]$type)
                 if(item_type == "table"){
-                    limitstring <- NA
-                    if( nrow(item_content) > rowlimit){
-                        limitstring <- sprintf(
-                            "Showing %i of %i observations",
-                            rowlimit,
-                            nrow(item_content)
-                        )
-                        item_content <- item_content[seq_len(rowlimit),]
-                    }
-                    string <- self$table(item_content, limitstring)
+                    string <- self$table_handle(item_content, rowlimit)
                 } else {
                     string <- self[[item_type]](item_content)
                 }
@@ -42,6 +25,27 @@ render <- R6::R6Class(
             }
             
             self$strings = unlist(strings, use.names = FALSE)
+        },
+        
+        class_handle = function(class){
+            if(class %in% self$valid_content_types){
+                return(class)
+            }else{
+                return(not_yet_implemented)
+            }
+        },
+        
+        table_handle = function(item_content, rowlimit){
+            limitstring <- NA
+            if( nrow(item_content) > rowlimit){
+                limitstring <- sprintf(
+                    "Showing %i of %i observations",
+                    rowlimit,
+                    nrow(item_content)
+                )
+                item_content <- item_content[seq_len(rowlimit),]
+            }
+            string <- self$table(item_content, limitstring)
         },
         
         
@@ -54,21 +58,8 @@ render <- R6::R6Class(
                     getR6Class(self)
                 )
             )
-        },
+        }
         
-        
-        ## Content functions - Implemented in the individual classes
-        h1 = function(x) self$not_yet_implemented("h1"),
-        h2 = function(x) self$not_yet_implemented("h2"),
-        h3 = function(x) self$not_yet_implemented("h3"),
-        h4 = function(x) self$not_yet_implemented("h4"),
-        p = function(x) self$not_yet_implemented("p"),
-        br = function(x) self$not_yet_implemented("br"),
-        table = function(x, limitstring) self$not_yet_implemented("table"),
-        
-        ## Output functions - Implemented in the individual classes
-        display = function() self$not_yet_implemented("display"),
-        file = function(filename) self$not_yet_implemented("file")
          
     )
 )
