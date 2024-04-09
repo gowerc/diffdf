3ce8bb876dd9041b073bd246c85e41cc5e3a6992
Kieran Martin <kieran.martin@roche.com>
2024-04-09 16:01:38 +0000

    fix lints

--- R/print.R
+++ R/print.R
@@ -13,39 +13,32 @@
 #' print( COMPARE )
 #' print( COMPARE, row_limit = 5 )
 #' @export
-print.diffdf <- function(x, row_limit = 10, as_string = FALSE, ... ) {
-    
-    if(!is.null(row_limit)){
-        if(length(row_limit) != 1){
+print.diffdf <- function(x, row_limit = 10, as_string = FALSE, ...) {
+    if (!is.null(row_limit)) {
+        if (length(row_limit) != 1) {
             stop("row_limit should have a length of 1")
         }
-        
-        if(!is.numeric(row_limit)){
+        if (!is.numeric(row_limit)) {
             stop("row_limit should be a numeric value or NULL")
         }
-        if(row_limit <= 0){
+        if (row_limit <= 0) {
             stop("row_limit should be a positive integer")
         }
-
     }
-    if(!is.logical(as_string) | length(as_string) != 1){
+    if (!is.logical(as_string) || length(as_string) != 1) {
         stop("as_string should be a logical of length one")
     }
-    
     COMPARE <- x
 
     if (length(COMPARE) == 0) {
         outtext <- "No issues were found!\n"
     } else {
-
         start_text <- paste0(
             "Differences found between the objects!\n\n",
             "A summary is given below.\n\n"
         )
-
         end_text <- lapply(COMPARE, function(x) get_print_message(x, row_limit))
         end_text <- paste0(unlist(end_text), collapse = "")
-
         outtext <- paste0(start_text, end_text)
     }
     if (as_string) {
